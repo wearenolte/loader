aa3509c2230846518e2c8b311cb1b2b73c256cbb
mitogh <mitogh@gmail.com>
2016-01-25 08:57:24 -0600

    Register loader functions

---
+++ src/Helpers/Error.php
@@ -0,0 +1,24 @@
+<?php namespace Leean\Helpers;
+
+/**
+ * Error class that will add warnings into the PHP but withoth breaking the page.
+ *
+ * @since 0.1.0
+ */
+class Error {
+	/**
+	 * This functions adds a warning message withoth braking the site or causing
+	 * a PHP error on the site when a file does not exist.
+	 *
+	 * @since 0.1.0
+	 *
+	 * @param string $path The file path to be used on the warning message.
+	 */
+	public static function file_not_found( $path ) {
+		$message = sprintf( '<code>%s</code> does not exist.', $path );
+		$allowed_html = [
+			'code' => [],
+		];
+		_doing_it_wrong( __FUNCTION__, wp_kses( $message, $allowed_html ), '4.3.1' );
+	}
+}
---
+++ src/Helpers/FileSystem.php
@@ -0,0 +1,64 @@
+<?php namespace Leean\Helpers;
+
+/**
+ * Class that handle all the logic related with file system and search files
+ * inside of the theme path.
+ *
+ * @since 0.1.0
+ */
+class FileSystem {
+
+	private $alias = [];
+	private $directories = [];
+	private $file_name = '';
+	private $type = '';
+
+	public function __construct( $file_name = '', $type = '' ){
+		$this->file_name = $file_name;
+		$this->type = $type;
+		$this->apply_filters();
+		$this->format();
+	}
+
+	private function apply_filters(){
+		$this->alias = apply_filters( 'loader_alias', [] );
+		$this->directories = apply_filters( 'loader_directories', [] );
+	}
+
+	public function get_path() {
+		$path = $this->search();
+		if ( file_exists( $path ) ) {
+			return $path;
+		} else {
+			Error::file_not_found( $path );
+			return false;
+		}
+	}
+
+	public function search() {
+		$path = false;
+		$alias = $this->get_alias();
+		foreach( $this->directories as $directory ) {
+			$path = sprintf(
+				'%s/%s/%s.php',
+				untrailingslashit( $directory ),
+				$alias,
+				$this->file_name
+			);
+			if ( file_exists( $path ) ) {
+				break;
+			}
+		}
+		return $path;
+	}
+
+	private function get_alias(){
+		return isset( $this->alias[ $this->type ] ) ? $this->alias[ $this->type ] : $this->type;
+	}
+
+	private function format() {
+		if ( false !== strpos( $this->file_name, '.php' ) ) {
+			$this->file_name = str_replace( '.php', '' );
+		}
+	}
+}
