1df5f51c4804588435b194444217477d295a8534
mitogh <mitogh@gmail.com>
2016-01-25 21:05:49 -0600

    Fix CI issues

--- src/Load.php
+++ src/Load.php
@@ -2,10 +2,25 @@
 
 use Leean\Helpers\FileSystem;
 
+/**
+ * Class that is responsible for handle all the logic behind the loading
+ * using __callStatic to generate dynamic getters.
+ *
+ * @since 0.1.0
+ */
 class Load {
 
+	/**
+	 * This method allow dynamic creation of custom getter so we can load anything
+	 * like for example: Load::myDirectoryisFunny( 'sample', $args );
+	 *
+	 * @since 0.1.0
+	 *
+	 * @param string $type The type of file to be loaded.
+	 * @param array  $arguments The arguments frmo the static call: file and args.
+	 */
 	public static function __callStatic( $type, $arguments ) {
-		if ( empty ( $arguments ) ) {
+		if ( empty( $arguments ) ) {
 			return;
 		}
 		$file = $arguments[0];
@@ -13,7 +28,17 @@ class Load {
 		self::loader( $file, $type, $args );
 	}
 
-	private static function loader( $file_name = '', $type = '', $args = [] ){
+	/**
+	 * Function that creates an object of the FileSystem in order to search the file
+	 * and pass the arguments to the file.
+	 *
+	 * @since 0.1.0
+	 *
+	 * @param string $file_name The name of the file.
+	 * @param string $type The type to be loaded.
+	 * @param array  $args The group of arguments to pass to the template or partial.
+	 */
+	private static function loader( $file_name = '', $type = '', $args = [] ) {
 		$files = new FileSystem( $file_name, $type );
 		$path = $files->get_path();
 		$files = null;
